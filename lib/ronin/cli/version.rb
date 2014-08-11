a56edcc24621e6511791ebe05eb89fa810db8c1b
Postmodern <postmodern.mod3@gmail.com>
2014-08-10 18:23:28 -0700

    Added ronin/cli/version.rb.

---
+++ lib/ronin/cli/version.rb
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2006-2014 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# Ronin CLI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ronin CLI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ronin CLI.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module CLI
+    # ronin-cli version
+    VERSION = '1.0.0'
+  end
+end
