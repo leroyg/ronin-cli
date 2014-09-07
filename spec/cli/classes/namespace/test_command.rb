48472bc4e1aa5a778b061cf159b20e89674e2aad
Postmodern <postmodern.mod3@gmail.com>
2014-09-07 15:15:46 -0700

    Forgot to add the Namespace::TestCommand for the specs.

---
+++ spec/cli/classes/namespace/test_command.rb
@@ -0,0 +1,13 @@
+require 'ronin/cli/command'
+
+module Ronin
+  module CLI
+    module Commands
+      module Namespace
+        class TestCommand < Command
+
+        end
+      end
+    end
+  end
+end
